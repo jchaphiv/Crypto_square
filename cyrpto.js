da92de11af18bdeb59c38bdbd7f43c13bdb4e89a
jchaphiv <jchaphiv@gmail.com>
2016-04-07 12:29:15 -0400

    Update cyrpto.js

--- cyrpto.js
+++ cyrpto.js
@@ -6,12 +6,22 @@ var Crypto = function (text){
 };
 
 Crypto.prototype.normalizePlaintext = function(){
-	return this.text.replace(/[^0-9a-zA-Z]/g, ''); 
-// replace(/[^0-9a-zA-Z]/g, ''): replaces all characters that are not digits and alpha characters
-	return this.text.toLowerCase();
+	var norm = this.text.replace(/[^0-9a-zA-Z]/g, '');
+	// replace(/[^0-9a-zA-Z]/g, ''): replaces all characters that are not digits and alpha characters.
+	return norm.toLowerCase();
+	// makes all capitalized letters in a string into lower case.
 };
 
+Crypto.prototype.size = function(){
+	var space = this.text.replace(/\s/g, '');
+	var count = space.length;
+	var sqr = Math.sqrt(count);
+	return Math.ceil(sqr);
+};
+
+Cyrpto.prototype.plaintextSegments = function(){
+
+};
+
+
 module.exports = Crypto;
-
-
-
